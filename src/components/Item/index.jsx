03a3b1adc0e73b46c02d3b1e395a26eedd90d12e
Frank <frank840629@gmail.com>
2022-02-05 18:22:23 +0800

    Add Item onMouseEnter/Leave

--- src/components/Item/index.jsx
+++ src/components/Item/index.jsx
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import './index.css';
 
 export default class Item extends Component {
+
+  state = {mouse:false}
+
+  handleMouse = (flag)=>{
+    return ()=>{
+      this.setState({mouse:flag})
+    }
+  }
+
   render() {
     const {id,name,done} = this.props
+    const {mouse} = this.state
     return(
-      <li>
+      <li style={{backgroundColor:mouse?'#ddd':'white'}} onMouseEnter={this.handleMouse(true)} onMouseLeave={this.handleMouse(false)}>
         <label>
           <input type="checkbox" defaultChecked={done}/>
           <span>{name}</span>
         </label>
-        <button className="btn btn-danger" style={{display:'none'}}>刪除</button>
+        <button className="btn btn-danger" style={{display:mouse?'block':'none'}}>刪除</button>
       </li>
     ) 
   }
